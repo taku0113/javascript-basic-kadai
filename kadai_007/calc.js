7843cc6f833840e6df454e3502d0eef60ef3449d
taku0113 <161075384+taku0113@users.noreply.github.com>
2024-04-02 23:09:41 +0900

    課題７アップ

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = 16;
+if (num % 3 == 0) {
+    console.log('３の倍数です');
+}
+
+if (num % 5 == 0) {
+    console.log('5の倍数です');
+}
+
+if (num % 15 == 0) {
+    console.log('3と5の倍数です');
+}
+else{
+    console.log(num);
+}

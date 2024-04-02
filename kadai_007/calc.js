a4476779d5d9ca08ab3e6754fbff0af79cf71444
taku0113 <161075384+taku0113@users.noreply.github.com>
2024-04-03 06:39:15 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
 let num = 16;
-if (num % 3 == 0) {
-    console.log('３の倍数です');
-}
-
-if (num % 5 == 0) {
-    console.log('5の倍数です');
-}
-
-if (num % 15 == 0) {
-    console.log('3と5の倍数です');
-}
-else{
-    console.log(num);
+switch (true) {
+    case num % 3 == 0 && num % 5 == 0:
+            console.log('3と5の倍数です');
+        break;
+     case num % 3 == 0:   
+        console.log('3の倍数です');
+        break;
+    case num % 5 == 0:
+    console.log ('５の倍数です');
+    break;
+    default:
+        console.log(num);
+        break;          
 }

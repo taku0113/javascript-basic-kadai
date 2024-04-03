160d373a507edc32f67277ca0452760302dd669b
taku0113 <161075384+taku0113@users.noreply.github.com>
2024-04-03 21:16:49 +0900

    ７アップ

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,16 @@
-let num = 16;
-switch (true) {
-    case num % 3 == 0 && num % 5 == 0:
-            console.log('3と5の倍数です');
-        break;
-     case num % 3 == 0:   
-        console.log('3の倍数です');
-        break;
-    case num % 5 == 0:
-    console.log ('５の倍数です');
-    break;
-    default:
-        console.log(num);
-        break;          
+let num = 5;
+if (num % 5 == 0 && num % 3 ==0) {
+    console.log('３と５の倍数です');
+}
+
+else if(num % 3 == 0) {
+    console.log('３の倍数です');
+}
+
+else if(num % 5 == 0) {
+    console.log('５の倍数です');
+}
+
+else {
+    console.log(num);
 }
